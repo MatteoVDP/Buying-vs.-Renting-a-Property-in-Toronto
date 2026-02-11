97bbfff73127fd8baf7dd27b710b3e24f3904e2e
matteo.vanderplaat <matteo.vanderplaat@gmail.com>
2026-02-11 18:11:40 +0000

    updated preprocessing file

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -25,6 +25,34 @@ df = clean_col_names(df)
 # Remove rows with all NaN values
 df.dropna(how='all', inplace=True)
 
+cols_to_smooth = [
+    'market_price_target_average,_detached_single_family_homes',
+    'national_pop',
+    'provincial_pop',
+    'municipal_pop',
+    'national_gdp_real,_seasonally_adjusted',
+    'provincial_gdp_real,_seasonally_adjusted',
+    'national_gdp_nominal',
+    'provincial_gdp_nominal',
+    'gdp_per_cap_real',
+    'most_recent_quarterly_gdp_%_change_extended',
+    'median_income_per_household_in_toronto',
+    'national_debt_to_gdp',
+    'provincial_debt_to_gdp',
+    'ontario_net_international_migration_monthly',
+    'ontario_net_interprovincial_migration_monthly',
+    'ontario_net_non_permanent_residents',
+    'housing_starts_sfh,_monthly',
+    'under_construction_sfh,_monthly',
+    'completions__sfh,_monthly'
+]
+
+for col in cols_to_smooth:
+    if col in df.columns:
+        df[col] = df[col].interpolate(method='linear')
+    else:
+        print(f"Warning: Column '{col}' not found in dataframe.")
+
 df['Log_Price'] = np.log(df['market_price_target_average,_detached_single_family_homes'])
 
 df['GDP_Growth_YoY'] = df['national_gdp_real,_seasonally_adjusted'].pct_change(12)
@@ -34,9 +62,9 @@ df['Municipal_Pop_Growth_YoY'] = df['municipal_pop'].pct_change(12)
 df['Inflation_Rate_YoY'] = df['cpi___national,_all_products'].pct_change(12)
 df['Labour_Force_Growth_YoY'] = df['total_labour_force_size_people_15_or_over,_employed_or_actively_seeking_work'].pct_change(12)
 df['Inflation_Rate'] = df['cpi___national,_core'].pct_change(12)
+df['Income_Growth_YoY'] = df['median_income_per_household_in_toronto'].pct_change(12)
 
-df['Real_Income'] = pd.to_numeric(df['median_income_per_household_in_toronto']) / df['cpi___national,_all_products']
-df['Migration_Rate'] = (df['ontario_net_interprovincial_migration_monthly'] + df['ontario_net_international_migration_monthly'])/df['provincial_pop']
+df['Migration_Rate'] = (df['ontario_net_interprovincial_migration_monthly'] + df['ontario_net_international_migration_monthly'] - df['ontario_net_non_permanent_residents'])/df['provincial_pop']
 df['NPR_Rate'] = df['ontario_net_non_permanent_residents']/df['provincial_pop']
 df['housing_starts_per_cap'] = df['housing_starts_sfh,_monthly']/df['municipal_pop']
 df['under_construction_per_cap'] = df['under_construction_sfh,_monthly']/df['municipal_pop']
@@ -65,6 +93,7 @@ columns_to_drop = [
     'cpi___national,_all_products',
     'total_labour_force_size_people_15_or_over,_employed_or_actively_seeking_work',
     'id',
+    'temp',
     'sales_volume',
     'year',
     'month',
@@ -91,4 +120,4 @@ columns_to_drop = [
 # Drop the specified columns from the dataframe
 df.drop(columns=columns_to_drop, inplace=True, axis=1)
 
-df.to_clipboard()
+df.to_csv("data/processed_data.csv", index=True)
